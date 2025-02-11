355aacdfd974d331d8d16d46aa24bbd5f31e98f6
clouqs <zetrox.gota@gmail.com>
2025-02-11 23:07:20 +0100

    start js

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World"); // stampa "Hello World" sulla console
+console.log("I like pizza");
+
+window.alert("Hello World"); // mostra un alert con il testo "Hello World"
+window.alert("I like pizza");
+
+//questo è un commento :)
+//console.log("Hello World");
+/* questo anche è un commento */
+
+document.getElementById("myH1").textContent = "Hello!!"
+document.getElementById("myP").textContent = "I like pizza" //il testo di js è più forte di quello di html
