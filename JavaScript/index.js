5890fc1d23e1ba43badfa1749ee96a63db83089c
clouqs <zetrox.gota@gmail.com>
2025-02-12 15:10:39 +0100

    remove old files and add counter program

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+let DecreaseBtn = document.getElementById('Decrease');
+let ResetBtn = document.getElementById('Reset');
+let IncreaseBtn = document.getElementById('Increase');
+let countLabel = document.getElementById('countLabel');
+let count = 0;
+
+IncreaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
+
+DecreaseBtn.onclick = function() {
+    count--;
+    if (count < 0) {
+        alert("You can't go below 0!");
+        count = 0;
+    }
+    countLabel.textContent = count; // Update after checking
+};
+
+ResetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+} 
