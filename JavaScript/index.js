0013f2043577e149c85a04b1965594db98dc223b
clouqs <zetrox.gota@gmail.com>
2025-02-13 08:49:07 +0100

    rand num gen

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,24 +1,5 @@
-let DecreaseBtn = document.getElementById('Decrease');
-let ResetBtn = document.getElementById('Reset');
-let IncreaseBtn = document.getElementById('Increase');
-let countLabel = document.getElementById('countLabel');
-let count = 0;
-
-IncreaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
-}
-
-DecreaseBtn.onclick = function() {
-    count--;
-    if (count < 0) {
-        alert("You can't go below 0!");
-        count = 0;
-    }
-    countLabel.textContent = count; // Update after checking
+document.getElementById("btn").onclick = function() {
+    let randNum = Math.random();
+    console.log(randNum);
+    document.getElementById("number").textContent = randNum;
 };
-
-ResetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
-}
