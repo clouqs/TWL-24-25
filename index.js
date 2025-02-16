89e1c05e8ec2028ade2c8320db704361854e8523
clouqs <zetrox.gota@gmail.com>
2025-02-16 17:31:34 +0100

    ex 7 - da copiare

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let numbers = [1, 2, 3, 4, 5]
-let maximum = Math.max(...numbers) // spread operator to pass the array as individual arguments
-
-
+class studente{
+    constructor(nome,cognome,matricola){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+    }
+    result(){
+        /*let newParagraph = document.createElement("p");
+        newParagraph.textContent = this.nome + this.cognome + this.matricola;
+        document.body.appendChild(newParagraph); */
+        document.body.innerHTML += `<p>  ${this.nome}  ${this.cognome}  ${this.matricola}  </p>`;
+    }
+}
+const studente1 = new studente("Mario ","Rossi ",12345);
+studente1.result();
