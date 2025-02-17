9d6b5c97fbcb074a2af1a30dc3c5122d80ff4a99
clouqs <zetrox.gota@gmail.com>
2025-02-17 08:40:21 +0100

    push

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-let numbers = [1, 2, 3, 4, 5]
-let maximum = Math.max(...numbers) // spread operator to pass the array as individual arguments
+//objects
+
+let persona ={
+    nome: "Mario",
+    età: 25,
+    città: "Roma"
+}
+
+console.log(persona.città);
+
+//fib
+let fib = [0, 1];
+
+for (let i = 2; i < 100; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+console.log(fib);
 
 
+//this 
+const persona2 = {
+    name: "skibidi",
+    favfood: "pizza",
+    sayhello: function(){console.log("hello, my name is " + this.favfood)}
+}
+
+persona2.sayhello();
